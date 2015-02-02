c49c15940cd820d922b494cf7cfa5920e92f9413
Cody Boggs <cody@formationds.com>
2015-02-02 07:56:07 -0700

    added influxdb args. added hostnames to docker run script. moved influxdb work to function and added optional create_db logic

--- gmond-influxdb-bridge.py
+++ gmond-influxdb-bridge.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 
 import argparse
+import influxdb
 import json
 import socket
 import time
-from influxdb import InfluxDBClient
-from io import StringIO, BytesIO
 from lxml import etree
+from os import path
 from telnetlib import Telnet
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('-d', '--debug', help="Set debug level - the higher the level, the further down the rabbit hole...")
-argparser.add_argument('-H', '--hosts', help="Comma-separated list of gmond hosts to poll")
+argparser.add_argument('-H', '--hosts', help="Comma-separated list of gmond hosts/IPs to poll")
 argparser.add_argument('-T', '--timeout', help="Connection timeout in seconds")
+argparser.add_argument('--db_host', help="InfluxDB hostname/IP")
+argparser.add_argument('--db_port', help="InfluxDB port")
+argparser.add_argument('--db_user', help="InfluxDB username")
+argparser.add_argument('--db_pass', help="InfluxDB password")
+argparser.add_argument('--db_name', help="InfluxDB database name")
+argparser.add_argument('--create_db', help="If db_name does not exist at db_host, create it (requires that db_user and db_pass be admin credentials)", action="store_true")
+argparser.add_argument('-I', '--interval', default=15, help="Polling interval in seconds")
 
 argparser.parse_args()
 args = argparser.parse_args()
-
-if args.hosts:
-    gmond_hosts = args.hosts.split(',')
-else:
-    ERR ("--hosts is required.")
 
 def D(level, msg):
     if args.debug and int(args.debug) >= level:
@@ -73,7 +75,8 @@ def parse_metrics(xml_data, payload):
         metrics.add(metric_elem.attrib['NAME'])
 
     # Dump any metrics are blacklisted (usually just values that aren't number types)
-    metrics = metrics.difference(metrics_blacklist)
+    metrics -= metrics_blacklist
+    D (2, "metrics found: {0}\n{1}".format(len(metrics), metrics))
 
     for metric_name in metrics:
         metric_data = {'name': metric_name, 'columns': columns, 'points': [] }
@@ -84,8 +87,47 @@ def parse_metrics(xml_data, payload):
             group = next(metric_elem.iterfind("EXTRA_DATA/EXTRA_ELEMENT[@NAME='GROUP']")).attrib['VAL']
             host_name=metric_elem.getparent().attrib['NAME']
             points = [cluster_name, host_name, metric_value, group, epoch_time]
+            D (3, "host={0} : group={1} : metric_name={2} : metric_value={3}".format(host_name, group, metric_name, metric_value))
             metric_data['points'].append(points)
         payload.append(metric_data)
+
+def push_metrics(db_host, db_port, db_user, db_pass, db_name, payload, create_db=False):
+    D (1, "create_db: {0}".format(args.create_db))
+    D (1, "db_host={0} : db_port={1} : db_user={2} : db_pass={3} : db_name={4}".format(db_host, db_port, db_user, db_pass, db_name))
+    client = influxdb.InfluxDBClient(db_host, db_port, db_user, db_pass, db_name)
+
+    if create_db:
+        try:
+            client.create_database(db_name)
+        except influxdb.client.InfluxDBClientError:
+            D (1, "db {0} already exists, skipping creation".format(db_name))
+        except:
+            ERR ("could not create database {0} - most likely you haven't provided admin credentials".format(db_name))
+        else:
+            INFO ("db {0} not found, creating".format(db_name))
+            INFO ("created user {0} in db {1}".format(db_user, db_name))
+    try:
+        client.write_points(payload, 's')
+    except:
+        ERR ("could not write data points to InfluxDB")
+    else:
+        D (1, "successfully wrote data points to InfluxDB")
+    
+if args.hosts:
+    gmond_hosts = args.hosts.split(',')
+else:
+    ERR ("--hosts is required.")
+    exit(1)
+
+if not (args.db_host and args.db_port and args.db_user and args.db_pass and args.db_name):
+    ERR ("ALL InfluxDB args are required - please consult {0} --help".format(path.basename(__file__)))
+    exit(1)
+else:
+    db_host = args.db_host
+    db_port = args.db_port
+    db_user = args.db_user
+    db_pass = args.db_pass
+    db_name = args.db_name
 
 epoch_time = int(time.time())
 columns = ["cluster", "hostname", "value", "group", "time"]
@@ -97,12 +139,19 @@ if args.timeout:
 else:
     timeout = 3
 
-D (1, "Hosts: {0}".format(gmond_hosts))
+INFO ("bridge starting up")
+D (1, "epoch time: {0}".format(epoch_time))
+D (1, "hosts: {0}".format(gmond_hosts))
+D (1, "blacklisted metrics: {0}".format(metrics_blacklist))
+D (1, "columns: {0}".format(columns))
 
 for host in gmond_hosts:
     xml_data = get_xml_data(host, timeout)
+    if xml_data:
+        parse_metrics(xml_data, payload)
+    else:
+        WARN ("no data found on host {0}".format(host))
 
-
-#client = InfluxDBClient('192.168.59.103', 58086, 'root', 'root', 'test')
-#client.create_database('test')
-#client.write_points(payload)
+if len(payload):
+    D (1, "pushing metrics to InfluxDB")
+    push_metrics(db_host, db_port, db_user, db_pass, db_name, payload, args.create_db)
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ p() {
 }
 
 p "Running cboggs-influxdb container...\n"
-docker run -d --name cboggs-influxdb -p 58083:8083 -p 58086:8086 cboggs-influxdb
+docker run -d --name cboggs-influxdb -h cboggs-influxdb -p 58083:8083 -p 58086:8086 cboggs-influxdb
 
 p "\nRunning cboggs-influxdb-bridge container...\n" 
-docker run -d --name cboggs-influxdb-bridge --link cboggs-influxdb:influxdb -p 8649:8649 cboggs-influxdb-bridge
+docker run -d --name cboggs-influxdb-bridge -h cboggs-influxdb-bridge --link cboggs-influxdb:influxdb -p 8649:8649 cboggs-influxdb-bridge
